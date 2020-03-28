d877506c03a67efe0064b6d8425dbfe1b26bf440
StrikerKagami <strikerkagami@gmail.com>
2020-03-28 13:25:41 +0000

    Basic Mechanics Development

---
+++ Assets/Actor/Shots/Shot_Master.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Shot_Master : MonoBehaviour
+{
+    public Rigidbody RB;
+    bool IsShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsShot)
+        {
+
+            transform.Translate(new Vector3(0, 0f, .2f));
+        }
+    }
+
+    public void OnShot(){
+        IsShot = true;
+
+    }
+
+}
